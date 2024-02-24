b1671925791f151ed57880a2a48d609ef2d2f9b8
Disha Gohil <123617662+Disha-2292@users.noreply.github.com>
2024-02-24 18:22:00 +0000

    Updated

--- c_concept_program/structure.c
+++ c_concept_program/structure.c
@@ -40,3 +40,42 @@ int main()
 
     return 0;
 }
+
+
+
+/*union
+#include <stdio.h>
+union stud{
+    int rollno;
+    char name[10];
+    float per;
+}s1,s2;
+int main()
+{
+    printf("Enter your roll number\n");
+    scanf("%d",&s1.rollno);
+    printf("Roll no is %d\n",s1.rollno);
+    
+    printf("Enter your name\n");
+    scanf("%s",&s1.name);
+    printf("Name is %s\n",s1.name);
+    
+    printf("Enter your percentage\n");
+    scanf("%f",&s1.per);
+    printf("Percentage is %f\n",s1.per);
+    
+    printf("Enter your roll number\n");
+    scanf("%d",&s2.rollno);
+    printf("Roll no is %d\n",s2.rollno);
+    
+    printf("Enter your name\n");
+    scanf("%s",&s2.name);
+    printf("Name is %s\n",s2.name);
+    
+    printf("Enter your percentage\n");
+    scanf("%f",&s2.per);
+    printf("Percentage is %f\n",s2.per);
+
+    return 0;
+}
+*/
